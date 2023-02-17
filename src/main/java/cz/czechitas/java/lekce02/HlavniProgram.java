a5d06dda7b786d1f45136b3a7ab3c9665544d488
Jana5642 <jancaurbanova@seznam.cz>
2023-02-17 21:11:40 +0100

    Metoda start() je veřejná.

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -7,23 +7,80 @@ public class HlavniProgram {
     public void start() {
         zofka = new Turtle();
 
+        for (int i = 0; i < 4; i++) {
+            zofka.turnRight(90);
+            zofka.move(100);
+        }
 
 
-
-
-
-           zofka.turnRight(90);
-           zofka.move(100);
-             zofka.turnRight(90);
-             zofka.move(100);
-             zofka.turnRight(90);
-             zofka.move(100);
-             zofka.turnRight(90);
-             zofka.move(100);
              zofka.turnRight(30);
              zofka.move(100);
         zofka.turnRight(120);
         zofka.move(100);
+
+        zofka.penUp();
+        zofka.turnRight(30);
+        zofka.move(100);
+        zofka.turnRight(90);
+        zofka.move(400);
+
+        zofka.penDown();
+
+            zofka.turnRight(90);
+            zofka.move(100);
+            zofka.turnRight(90);
+            zofka.move(130);
+        zofka.turnRight(90);
+        zofka.move(100);
+        zofka.turnRight(90);
+        zofka.move(130);
+
+        zofka.turnRight(30);
+        zofka.move(100);
+        zofka.turnRight(120);
+        zofka.move(100);
+
+        zofka.turnRight(120);
+        zofka.penUp();
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(32);
+//nozicky
+        zofka.penDown();
+        zofka.turnRight(120);
+        zofka.move(30);
+        zofka.turnRight(180);
+        zofka.move(30);
+        zofka.turnRight(120);
+        zofka.move(30);
+
+        zofka.penUp();
+        zofka.turnRight(180);
+        zofka.move(30);
+
+        zofka.turnRight(120);
+        zofka.move(64);
+
+        //nozicky
+        zofka.penDown();
+        zofka.turnRight(120);
+        zofka.move(30);
+        zofka.turnRight(180);
+        zofka.move(30);
+        zofka.turnRight(120);
+        zofka.move(30);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
